f6b949e90a34438e26f8e46ebe28251712bf7551
Jacobtread <jacobtread@gmail.com>
2023-09-16 13:18:41 +1200

    Support for chakram launcher and higher polonium round tier

--- src/lib/components/inventory/InventoryWeapons.svelte
+++ src/lib/components/inventory/InventoryWeapons.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
-    import { isAdmin, player } from "$lib/api/api";
-    import { MAX_WEAPON_LEVEL, WEAPON_CATEGORIES } from "$lib/data/inventory";
-    import InventoryList from "./InventoryList.svelte";
-    import LevelInput from "./LevelInput.svelte";
+  import { isAdmin, player } from "$lib/api/api";
+  import { MAX_WEAPON_LEVEL, WEAPON_CATEGORIES } from "$lib/data/inventory";
+  import InventoryList from "./InventoryList.svelte";
+  import LevelInput from "./LevelInput.svelte";
 
-    // Inventory values array
-    export let inventory: number[];
+  // Inventory values array
+  export let inventory: number[];
 </script>
 
 <InventoryList tabs={WEAPON_CATEGORIES} let:selected>
-    {#each selected.values as weapon}
-        <div class="card inv-card">
-            <img
-                src={`/assets/weapons/${weapon.image}`}
-                alt={weapon.name}
-                width={160}
-                height={120}
-            />
+  {#each selected.values as weapon}
+    <div class="card inv-card">
+      <img
+        class="inv-card-weapon"
+        src={`/assets/weapons/${weapon.image}`}
+        alt={weapon.name}
+        width={160}
+        height={120}
+      />
 
-            <div class="inv-card__value">
-                <h2>{weapon.name}</h2>
-                <span class="annot">
-                    {inventory[weapon.level_index]}
-                </span>
+      <div class="inv-card__value">
+        <h2>{weapon.name}</h2>
+        <span class="annot">
+          {inventory[weapon.level_index]}
+        </span>
 
-                {#if isAdmin($player)}
-                    <LevelInput
-                        bind:value={inventory[weapon.level_index]}
-                        min={0}
-                        max={MAX_WEAPON_LEVEL}
-                    />
-                {/if}
-            </div>
-        </div>
-    {/each}
+        {#if isAdmin($player)}
+          <LevelInput
+            bind:value={inventory[weapon.level_index]}
+            min={0}
+            max={MAX_WEAPON_LEVEL}
+          />
+        {/if}
+      </div>
+    </div>
+  {/each}
 </InventoryList>
--- src/lib/data/inventory.ts
+++ src/lib/data/inventory.ts
@@ -157,6 +157,7 @@ export const ASSAULT_RIFLES: Weapon[] = [
     { level_index: 626, rarity: Rarity.UltraRare, name: "Geth Spitfire", image: "AssaultRifle_Spitfire.webp" },
     { level_index: 623, rarity: Rarity.UltraRare, name: "M-7 Lancer", image: "AssaultRifle_Lancer_MP.webp" },
     { level_index: 627, rarity: Rarity.Rare, name: "Adas Anti-Synthetic Rifle", image: "AssaultRifle_Adas_MP.webp" },
+    { level_index: 289, rarity: Rarity.Rare, name: "Chakram Launcher", image: "AssaultRifle_Reckoning.webp" },
 ];
 
 export const SUB_MACHINE_GUNS: Weapon[] = [
@@ -487,7 +488,7 @@ export const AMMO_CONSUMABLES: TieredConsumable[] = [
     {
         name: "Polonium Rounds",
         image: "AmmoPower_Polonium.webp",
-        indexes: [515, 520, 525],
+        indexes: [515, 520, 525, 526],
         unused: true
     },
 ];

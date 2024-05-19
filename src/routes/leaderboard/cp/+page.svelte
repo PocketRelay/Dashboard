9e172ed433ac679166b03491c11667eb7ba60217
Jacobtread <jacobtread@gmail.com>
2024-05-19 14:54:13 +1200

    feat: public leaderboard pages

---
+++ src/routes/leaderboard/cp/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { getLeaderboard, LeaderboardName } from "$lib/api/leaderboard";
+  import Loader from "$lib/components/Loader.svelte";
+  import PageHeading from "$lib/components/PageHeading.svelte";
+  import { getNumberWithOrdinal } from "$lib/tools/numbers";
+  import { createQuery } from "@tanstack/svelte-query";
+
+  const query = createQuery({
+    queryKey: ["leaderboard-public-cp"],
+    queryFn: () => getLeaderboard(LeaderboardName.ChallengePoints, 0, 20),
+  });
+</script>
+
+<div class="leaderboard">
+  <PageHeading title="Leaderboard" text="Challenge Points" />
+
+  {#if $query.error}
+    <p class="error">{$query.error}</p>
+  {/if}
+
+  <div class="table-container">
+    <table class="table">
+      {#if $query.isLoading}
+        <Loader />
+      {/if}
+      <thead class="table__head">
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody class="table__body">
+        {#if $query.data}
+          {#each $query.data.entries as entry}
+            <tr class="table__entry">
+              <td class="annot">{getNumberWithOrdinal(entry.rank)}</td>
+              <td>{entry.player_name}</td>
+              <td class="annot">{entry.value}</td>
+            </tr>
+          {/each}
+        {/if}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<style lang="scss">
+  .leaderboard {
+    padding: 1rem;
+  }
+</style>

13dcd3ca90a151e5712a0e0ae04b542b205b0e40
Jacobtread <jacobtread@gmail.com>
2025-03-15 22:31:24 +1300

    feat: update links

--- src/lib/components/auth/RequestLoginCodeForm.svelte
+++ src/lib/components/auth/RequestLoginCodeForm.svelte
@@ -3,6 +3,7 @@
   import { base } from "$app/paths";
   import { requestLoginCode } from "$lib/api/auth";
   import Loader from "$lib/components/Loader.svelte";
+  import { CLIENT_DOWNLOAD_LINK, WEBSITE_LINK } from "$lib/data/pocket-relay";
   import { createMutation } from "@tanstack/svelte-query";
 
   // Form fields
@@ -57,7 +58,7 @@
   <div class="info">
     <a
       class="annot"
-      href="https://github.com/PocketRelay/Client/releases/latest/download/pocket-relay-client.exe"
+      href={CLIENT_DOWNLOAD_LINK}
       rel="noreferrer"
       target="_blank"
       title="Download the latest client version">Download Client</a
@@ -65,7 +66,7 @@
 
     <a
       class="annot"
-      href="https://pocket-relay.pages.dev/"
+      href={WEBSITE_LINK}
       rel="noreferrer"
       target="_blank"
       title="View the website for the Pocket Relay project"
---
+++ src/lib/data/pocket-relay.ts
@@ -0,0 +1,4 @@
+export const CLIENT_DOWNLOAD_LINK =
+  "https://github.com/PocketRelay/PocketRelayPluginInstaller/releases/latest/download/pocket-relay-plugin-installer.exe";
+
+export const WEBSITE_LINK = "https://pocket-relay.pages.dev/";

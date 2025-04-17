8da9913dbb217bfe7d7fa5f9a44c57312bfc6f88
N7Defect <18038432+N7Defect@users.noreply.github.com>
2025-04-17 02:19:43 +0200

    Missed from last merge

--- src/routes/(dashboard)/mpchallenges/+page.svelte
+++ src/routes/(dashboard)/mpchallenges/+page.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
   import { player } from "$lib/api/api";
+  import { getPlayerData } from "$lib/api/players";
+  import { getPlayerChallenges } from "$lib/data/parser/challenges";
+  import Loader from "$lib/components/Loader.svelte";
+  import {PLAYER_CHALLENGES_KEY} from "$lib/data/parser/challenges";
+  import { createQuery } from "@tanstack/svelte-query";
+  import { derived } from "svelte/store";
   import PageHeading from "$lib/components/PageHeading.svelte";
   import Challenges from "$lib/components/MpChallenges.svelte";
+
+  const query = createQuery(
+    derived(player, ($player) => ({
+      queryKey: [PLAYER_CHALLENGES_KEY, $player?.id],
+      queryFn: async () => {
+        if ($player === null) throw new Error("Not authenticated");
+
+        const response = await getPlayerData($player.id, PLAYER_CHALLENGES_KEY);
+        return getPlayerChallenges(response.value);
+      },
+    }))
+  );
+
 </script>
 
 <PageHeading title="MP Challenges"/>
-<Challenges playerId={$player.id} />
+{#if $query.error}
+  <p class="error">{$query.error}</p>
+{/if}
+
+{#if $query.isLoading}
+<Loader />
+{:else if $query.data}
+<Challenges playerChallenges={$query.data} />
+{:else}
+  <p>No challenges found</p>
+{/if}

82fb14e266d921e7aad8980d946c590e84941609
Jacobtread <jacobtread@gmail.com>
2025-11-21 22:18:19 +1300

    chore: reformat files

--- src/lib/components/inventory/characters/power/PowerEditor.svelte
+++ src/lib/components/inventory/characters/power/PowerEditor.svelte
@@ -1,247 +1,201 @@
 <script lang="ts">
-    import {
-        createDefaultPowerSelections,
-        type PlayerCharacterPower,
-        type PlayerData,
-    } from "$lib/data/parser";
-    import type { Power } from "$lib/data/powers";
-    import InventoryCharacterPowerLevel from "./PowerLevelDisplay.svelte";
-    import PowerEditorEvolution from "./PowerEditorEvolution.svelte";
+  import {
+    createDefaultPowerSelections,
+    type PlayerCharacterPower,
+    type PlayerData,
+  } from "$lib/data/parser";
+  import type { Power } from "$lib/data/powers";
+  import InventoryCharacterPowerLevel from "./PowerLevelDisplay.svelte";
+  import PowerEditorEvolution from "./PowerEditorEvolution.svelte";
 
-    export let playerData: PlayerData;
-    export let powerData: PlayerCharacterPower;
-    export let editable: boolean;
-    export let availablePoints: number;
+  export let playerData: PlayerData;
+  export let powerData: PlayerCharacterPower;
+  export let editable: boolean;
+  export let availablePoints: number;
 
-    $: power = powerData.power! as Power;
+  $: power = powerData.power! as Power;
 
-    enum Path {
-        A,
-        B,
-    }
+  enum Path {
+    A,
+    B,
+  }
 
-    function isAllowedProgress(
-        availablePoints: number,
-        currentProgress: number,
-        progress: number,
+  function isAllowedProgress(
+    availablePoints: number,
+    currentProgress: number,
+    progress: number
+  ) {
+    const usedPoints = (currentProgress * (currentProgress + 1)) / 2;
+    const nextPoints = (progress * (progress + 1)) / 2;
+
+    const freePoints = availablePoints + usedPoints;
+    const pointsAfter = freePoints - nextPoints;
+    return pointsAfter >= 0;
+  }
+
+  function onChoosePowerProgress(progress: number) {
+    if (
+      !isAllowedProgress(availablePoints, powerData.powerProgress, progress)
     ) {
-        const usedPoints = (currentProgress * (currentProgress + 1)) / 2;
-        const nextPoints = (progress * (progress + 1)) / 2;
-
-        const freePoints = availablePoints + usedPoints;
-        const pointsAfter = freePoints - nextPoints;
-        return pointsAfter >= 0;
+      return;
     }
 
-    function onChoosePowerProgress(progress: number) {
-        if (
-            !isAllowedProgress(
-                availablePoints,
-                powerData.powerProgress,
-                progress,
-            )
-        ) {
-            return;
-        }
-
-        if (progress === 1.0 && powerData.powerProgress === 1.0) {
-            // Selecting the first box twice will clear it
-            powerData.powerProgress = 0.0;
-        } else {
-            powerData.powerProgress = progress;
-        }
-
-        if (progress < 3.0) {
-            powerData.powerSelections = createDefaultPowerSelections();
-        }
-
-        playerData = playerData;
+    if (progress === 1.0 && powerData.powerProgress === 1.0) {
+      // Selecting the first box twice will clear it
+      powerData.powerProgress = 0.0;
+    } else {
+      powerData.powerProgress = progress;
     }
 
-    function getPathAtIndex(index: number): Path | undefined {
-        const [a, b] = powerData.powerSelections[index];
-
-        return a ? Path.A : b ? Path.B : undefined;
+    if (progress < 3.0) {
+      powerData.powerSelections = createDefaultPowerSelections();
     }
 
-    function setPathAtIndex(index: number, path: Path) {
-        powerData.powerSelections[index] =
-            path === Path.A ? [true, false] : [false, true];
+    playerData = playerData;
+  }
+
+  function getPathAtIndex(index: number): Path | undefined {
+    const [a, b] = powerData.powerSelections[index];
+
+    return a ? Path.A : b ? Path.B : undefined;
+  }
+
+  function setPathAtIndex(index: number, path: Path) {
+    powerData.powerSelections[index] =
+      path === Path.A ? [true, false] : [false, true];
+  }
+
+  function onChoosePowerPath(progress: number, index: number, path: Path) {
+    for (let i = 0; i < index; i += 1) {
+      const pathAt = getPathAtIndex(i);
+      if (pathAt === undefined) {
+        setPathAtIndex(i, path);
+      }
     }
 
-    function onChoosePowerPath(progress: number, index: number, path: Path) {
-        for (let i = 0; i < index; i += 1) {
-            const pathAt = getPathAtIndex(i);
-            if (pathAt === undefined) {
-                setPathAtIndex(i, path);
-            }
-        }
+    setPathAtIndex(index, path);
 
-        setPathAtIndex(index, path);
-
-        powerData.powerProgress = progress;
-        playerData = playerData;
-    }
+    powerData.powerProgress = progress;
+    playerData = playerData;
+  }
 </script>
 
 <div class="evolutions-wrapper">
-    <InventoryCharacterPowerLevel {powerData} />
+  <InventoryCharacterPowerLevel {powerData} />
 
-    <div class="evolutions">
-        <PowerEditorEvolution
-            index={0}
-            active={powerData.powerProgress > 0}
-            onClickEvolution={() => onChoosePowerProgress(1.0)}
-            {power}
-            editable={editable &&
-                isAllowedProgress(
-                    availablePoints,
-                    powerData.powerProgress,
-                    1.0,
-                )}
-        />
+  <div class="evolutions">
+    <PowerEditorEvolution
+      index={0}
+      active={powerData.powerProgress > 0}
+      onClickEvolution={() => onChoosePowerProgress(1.0)}
+      {power}
+      editable={editable &&
+        isAllowedProgress(availablePoints, powerData.powerProgress, 1.0)}
+    />
 
-        <PowerEditorEvolution
-            index={1}
-            active={powerData.powerProgress > 1}
-            onClickEvolution={() => onChoosePowerProgress(2.0)}
-            {power}
-            editable={editable &&
-                isAllowedProgress(
-                    availablePoints,
-                    powerData.powerProgress,
-                    2.0,
-                )}
-        />
+    <PowerEditorEvolution
+      index={1}
+      active={powerData.powerProgress > 1}
+      onClickEvolution={() => onChoosePowerProgress(2.0)}
+      {power}
+      editable={editable &&
+        isAllowedProgress(availablePoints, powerData.powerProgress, 2.0)}
+    />
 
-        <PowerEditorEvolution
-            index={2}
-            active={powerData.powerProgress > 2}
-            onClickEvolution={() => onChoosePowerProgress(3.0)}
-            {power}
-            editable={editable &&
-                isAllowedProgress(
-                    availablePoints,
-                    powerData.powerProgress,
-                    3.0,
-                )}
-        />
+    <PowerEditorEvolution
+      index={2}
+      active={powerData.powerProgress > 2}
+      onClickEvolution={() => onChoosePowerProgress(3.0)}
+      {power}
+      editable={editable &&
+        isAllowedProgress(availablePoints, powerData.powerProgress, 3.0)}
+    />
 
-        <div class="evolution-group">
-            <PowerEditorEvolution
-                index={3}
-                active={powerData.powerProgress > 3 &&
-                    getPathAtIndex(0) == Path.A}
-                onClickEvolution={() => onChoosePowerPath(4.0, 0, Path.A)}
-                {power}
-                editable={editable &&
-                    isAllowedProgress(
-                        availablePoints,
-                        powerData.powerProgress,
-                        4.0,
-                    )}
-            />
+    <div class="evolution-group">
+      <PowerEditorEvolution
+        index={3}
+        active={powerData.powerProgress > 3 && getPathAtIndex(0) == Path.A}
+        onClickEvolution={() => onChoosePowerPath(4.0, 0, Path.A)}
+        {power}
+        editable={editable &&
+          isAllowedProgress(availablePoints, powerData.powerProgress, 4.0)}
+      />
 
-            <PowerEditorEvolution
-                index={4}
-                active={powerData.powerProgress > 3 &&
-                    getPathAtIndex(0) == Path.B}
-                onClickEvolution={() => onChoosePowerPath(4.0, 0, Path.B)}
-                {power}
-                editable={editable &&
-                    isAllowedProgress(
-                        availablePoints,
-                        powerData.powerProgress,
-                        4.0,
-                    )}
-            />
-        </div>
-
-        <div class="evolution-group">
-            <PowerEditorEvolution
-                index={5}
-                active={powerData.powerProgress > 4 &&
-                    getPathAtIndex(1) == Path.A}
-                onClickEvolution={() => onChoosePowerPath(5.0, 1, Path.A)}
-                {power}
-                editable={editable &&
-                    isAllowedProgress(
-                        availablePoints,
-                        powerData.powerProgress,
-                        5.0,
-                    )}
-            />
-
-            <PowerEditorEvolution
-                index={6}
-                active={powerData.powerProgress > 4 &&
-                    getPathAtIndex(1) == Path.B}
-                onClickEvolution={() => onChoosePowerPath(5.0, 1, Path.B)}
-                {power}
-                editable={editable &&
-                    isAllowedProgress(
-                        availablePoints,
-                        powerData.powerProgress,
-                        5.0,
-                    )}
-            />
-        </div>
-        <div class="evolution-group">
-            <PowerEditorEvolution
-                index={7}
-                active={powerData.powerProgress > 5 &&
-                    getPathAtIndex(2) == Path.A}
-                onClickEvolution={() => onChoosePowerPath(6.0, 2, Path.A)}
-                {power}
-                editable={editable &&
-                    isAllowedProgress(
-                        availablePoints,
-                        powerData.powerProgress,
-                        6.0,
-                    )}
-            />
-
-            <PowerEditorEvolution
-                index={8}
-                active={powerData.powerProgress > 5 &&
-                    getPathAtIndex(2) == Path.B}
-                onClickEvolution={() => onChoosePowerPath(6.0, 2, Path.B)}
-                {power}
-                editable={editable &&
-                    isAllowedProgress(
-                        availablePoints,
-                        powerData.powerProgress,
-                        6.0,
-                    )}
-            />
-        </div>
+      <PowerEditorEvolution
+        index={4}
+        active={powerData.powerProgress > 3 && getPathAtIndex(0) == Path.B}
+        onClickEvolution={() => onChoosePowerPath(4.0, 0, Path.B)}
+        {power}
+        editable={editable &&
+          isAllowedProgress(availablePoints, powerData.powerProgress, 4.0)}
+      />
     </div>
+
+    <div class="evolution-group">
+      <PowerEditorEvolution
+        index={5}
+        active={powerData.powerProgress > 4 && getPathAtIndex(1) == Path.A}
+        onClickEvolution={() => onChoosePowerPath(5.0, 1, Path.A)}
+        {power}
+        editable={editable &&
+          isAllowedProgress(availablePoints, powerData.powerProgress, 5.0)}
+      />
+
+      <PowerEditorEvolution
+        index={6}
+        active={powerData.powerProgress > 4 && getPathAtIndex(1) == Path.B}
+        onClickEvolution={() => onChoosePowerPath(5.0, 1, Path.B)}
+        {power}
+        editable={editable &&
+          isAllowedProgress(availablePoints, powerData.powerProgress, 5.0)}
+      />
+    </div>
+    <div class="evolution-group">
+      <PowerEditorEvolution
+        index={7}
+        active={powerData.powerProgress > 5 && getPathAtIndex(2) == Path.A}
+        onClickEvolution={() => onChoosePowerPath(6.0, 2, Path.A)}
+        {power}
+        editable={editable &&
+          isAllowedProgress(availablePoints, powerData.powerProgress, 6.0)}
+      />
+
+      <PowerEditorEvolution
+        index={8}
+        active={powerData.powerProgress > 5 && getPathAtIndex(2) == Path.B}
+        onClickEvolution={() => onChoosePowerPath(6.0, 2, Path.B)}
+        {power}
+        editable={editable &&
+          isAllowedProgress(availablePoints, powerData.powerProgress, 6.0)}
+      />
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
-    .evolutions {
-        display: flex;
-        flex-flow: row;
-        gap: 1rem;
-        align-items: center;
-        overflow-x: auto;
-        width: 100%;
-        padding: 1rem;
-        border: 1px solid #444;
-        border-radius: 0.25rem;
-        margin-bottom: 1rem;
-    }
+  .evolutions {
+    display: flex;
+    flex-flow: row;
+    gap: 1rem;
+    align-items: center;
+    overflow-x: auto;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #444;
+    border-radius: 0.25rem;
+    margin-bottom: 1rem;
+  }
 
-    .evolutions-wrapper {
-        display: flex;
-        flex-flow: column;
-        gap: 2rem;
-        align-items: flex-start;
-    }
+  .evolutions-wrapper {
+    display: flex;
+    flex-flow: column;
+    gap: 2rem;
+    align-items: flex-start;
+  }
 
-    .evolution-group {
-        display: flex;
-        flex-flow: column;
-        gap: 1rem;
-    }
+  .evolution-group {
+    display: flex;
+    flex-flow: column;
+    gap: 1rem;
+  }
 </style>
--- src/lib/components/inventory/characters/power/PowerEditorEvolution.svelte
+++ src/lib/components/inventory/characters/power/PowerEditorEvolution.svelte
@@ -1,115 +1,115 @@
 <script lang="ts">
-    import { replaceTemplates, type Power } from "$lib/data/powers";
-    import { Tooltip } from "bits-ui";
+  import { replaceTemplates, type Power } from "$lib/data/powers";
+  import { Tooltip } from "bits-ui";
 
-    export let index: number;
-    export let editable: boolean;
+  export let index: number;
+  export let editable: boolean;
 
-    export let active: boolean;
-    export let power: Power;
-    export let onClickEvolution: VoidFunction;
+  export let active: boolean;
+  export let power: Power;
+  export let onClickEvolution: VoidFunction;
 
-    $: evolution = power.evolutions[index];
+  $: evolution = power.evolutions[index];
 </script>
 
 <Tooltip.Root openDelay={100}>
-    <Tooltip.Trigger>
-        <button
-            class="evolution"
-            class:evolution--active={active}
-            on:click={onClickEvolution}
-            disabled={!editable}
-        >
-            <div class="evolution-icon" data-index={index}>
-                <img
-                    src={`/assets/powers/${power.iconSet}.webp`}
-                    alt={evolution.name}
-                />
-            </div>
-            <p class="evolution-name">{evolution.name}</p>
-        </button>
-    </Tooltip.Trigger>
+  <Tooltip.Trigger>
+    <button
+      class="evolution"
+      class:evolution--active={active}
+      on:click={onClickEvolution}
+      disabled={!editable}
+    >
+      <div class="evolution-icon" data-index={index}>
+        <img
+          src={`/assets/powers/${power.iconSet}.webp`}
+          alt={evolution.name}
+        />
+      </div>
+      <p class="evolution-name">{evolution.name}</p>
+    </button>
+  </Tooltip.Trigger>
 
-    <Tooltip.Content>
-        <Tooltip.Arrow />
+  <Tooltip.Content>
+    <Tooltip.Arrow />
 
-        <p class="evolution-description">
-            {replaceTemplates(evolution.description, evolution.attributes)}
-        </p>
-    </Tooltip.Content>
+    <p class="evolution-description">
+      {replaceTemplates(evolution.description, evolution.attributes)}
+    </p>
+  </Tooltip.Content>
 </Tooltip.Root>
 
 <style lang="scss">
-    .evolution-description {
-        max-width: 300px;
-    }
+  .evolution-description {
+    max-width: 300px;
+  }
 
-    .evolution {
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        flex-flow: column;
-        border: 1px solid #444;
-        width: 10rem;
-        padding: 0.5rem;
-        flex-shrink: 0;
-        background-color: transparent;
+  .evolution {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    flex-flow: column;
+    border: 1px solid #444;
+    width: 10rem;
+    padding: 0.5rem;
+    flex-shrink: 0;
+    background-color: transparent;
 
-        &--active {
-            background-color: #333;
-        }
+    &--active {
+      background-color: #333;
     }
+  }
 
-    .evolution:disabled {
-        cursor: not-allowed;
-    }
+  .evolution:disabled {
+    cursor: not-allowed;
+  }
 
-    .evolution-name {
-        font-size: 0.75rem;
-        color: white;
-        text-align: center;
-    }
-    .evolution-icon {
-        height: 64px;
-        overflow: hidden;
-        width: 64px;
-    }
-    .evolution-icon > img {
-        height: 128px;
-        position: relative;
-        // Scale the image down 50%.
-        width: 384px;
-    }
+  .evolution-name {
+    font-size: 0.75rem;
+    color: white;
+    text-align: center;
+  }
+  .evolution-icon {
+    height: 64px;
+    overflow: hidden;
+    width: 64px;
+  }
+  .evolution-icon > img {
+    height: 128px;
+    position: relative;
+    // Scale the image down 50%.
+    width: 384px;
+  }
 
-    .evolution-icon[data-index="0"] > img {
-        left: 0;
-    }
-    .evolution-icon[data-index="1"] > img {
-        left: -64px;
-    }
-    .evolution-icon[data-index="2"] > img {
-        left: -128px;
-    }
-    .evolution-icon[data-index="3"] > img {
-        left: -192px;
-    }
+  .evolution-icon[data-index="0"] > img {
+    left: 0;
+  }
+  .evolution-icon[data-index="1"] > img {
+    left: -64px;
+  }
+  .evolution-icon[data-index="2"] > img {
+    left: -128px;
+  }
+  .evolution-icon[data-index="3"] > img {
+    left: -192px;
+  }
 
-    .evolution-icon[data-index="4"] > img {
-        left: -192px;
-        top: -64px;
-    }
-    .evolution-icon[data-index="5"] > img {
-        left: -256px;
-    }
-    .evolution-icon[data-index="6"] > img {
-        left: -256px;
-        top: -64px;
-    }
-    .evolution-icon[data-index="7"] > img {
-        left: -320px;
-    }
-    .evolution-icon[data-index="8"] > img {
-        left: -320px;
-        top: -64px;
-    }
+  .evolution-icon[data-index="4"] > img {
+    left: -192px;
+    top: -64px;
+  }
+  .evolution-icon[data-index="5"] > img {
+    left: -256px;
+  }
+  .evolution-icon[data-index="6"] > img {
+    left: -256px;
+    top: -64px;
+  }
+  .evolution-icon[data-index="7"] > img {
+    left: -320px;
+  }
+  .evolution-icon[data-index="8"] > img {
+    left: -320px;
+    top: -64px;
+  }
 </style>
